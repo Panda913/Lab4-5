a803ca8b2bd4ff2b6bd582ec7e8d12598d9e9737
Yuliia Hlushko <78353295+Panda913@users.noreply.github.com>
2023-06-01 00:18:12 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,43 @@
 
+# Оголошення провайдера для Terraform 
+provider "aws" {
+  access_key = "${{secrets.ACCESS_KEY}}"
+  secret_access_key = "${{secrets.SECRET_ACCESS_KEY}}"
+  region = "eu-north-1" 
+}
+
+# Створення Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web_security_group"
+  description = "Security Group"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+}
+
+# Створення VM instance
+resource "aws_instance" "web_server" {
+  ami           = "${{secrets.AMI_ID}}"
+  instance_type = "t2.micro" 
+
+
+  # Підключення Security Group до інстансу
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  # Установка Docker після створення інстансу
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y docker.io
+              sudo systemctl start docker
+              EOF
+}
+
+# Оголошення виводу публічної IP-адреси інстансу
+output "public_ip" {
+  value = aws_instance.web_server.public_ip
+}
