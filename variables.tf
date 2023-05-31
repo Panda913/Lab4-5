715f74754c4cf95a056d934e6951f33515418c73
Yuliia Hlushko <78353295+Panda913@users.noreply.github.com>
2023-06-01 00:39:54 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  description = "AWS ключ доступу"
+  type        = string
+}
+
+variable "secret_access_key" {
+  description = "AWS секретний ключ доступу"
+  type        = string
+}
